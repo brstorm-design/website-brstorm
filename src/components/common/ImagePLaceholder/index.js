81787310972e0facdb6446f59d075bd4bda12d56
luangelsdorf <luanferreira2136@gmail.com>
2021-08-15 14:41:03 -0300

    Fixed module name

--- src/components/common/ImagePLaceholder/index.js
+++ src/components/common/ImagePLaceholder/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from 'ImagePaceholder.module.scss';
+import styles from 'ImagePlaceholder.module.scss';
 import icon from '../../../../public/images/icon-image.svg';
 import Image from 'next/image';
 
-export default function ImagePaceholder({ width, height }) {
+export default function ImagePlaceholder({ width, height }) {
   return (
     <div className={styles.image} style={{width: width + 'px', height: height + 'px'}}>
       <Image src={icon} />
