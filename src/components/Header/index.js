5f86332653fbf7cd85d2b9f681fd694bbc08f35d
luangelsdorf <luanferreira2136@gmail.com>
2021-08-15 11:16:33 -0300

    Finalized Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,12 @@
+import Link from 'next/link';
 import React, { useEffect } from 'react';
+import Logo from '../common/Logo';
 import styles from './Header.module.scss';
 
 export default function Header() {
 
   useEffect(() => {
     const header = document.querySelector('header');
-
     window.onscroll = () => {
       if (window.pageYOffset > 24) {
         header.classList.add(styles.shrink);
@@ -18,7 +19,38 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
-      <span className="text-white">Header</span>
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+          <Link href="/">
+            <a>
+              <Logo />
+            </a>
+          </Link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" />
+          </button>
+          <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Solutions</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Projects & Reviews</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Advantages</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contacts</a>
+              </li>
+            </ul>
+          </div>
+          <a href="#" className="btn small">Book a Meeting</a>
+        </div>
+      </nav>
     </header>
   )
 }
---
+++ src/pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
