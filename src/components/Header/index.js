eff05152e63ca2f69b2ad547a2b63925ff96b9cc
luangelsdorf <luanferreira2136@gmail.com>
2021-08-14 23:53:25 -0300

    Done shrinking header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './Header.module.scss';
 
 export default function Header() {
+
+  useEffect(() => {
+    const header = document.querySelector('header');
+
+    window.onscroll = () => {
+      if (window.pageYOffset > 24) {
+        header.classList.add(styles.shrink);
+      }
+      else {
+        header.classList.remove(styles.shrink);
+      }
+    }
+  }, [])
+
   return (
     <header className={styles.header}>
       <span className="text-white">Header</span>
