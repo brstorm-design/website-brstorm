0c0bb9cfa2ac095858fbd82c721c21bcc92aa0ed
luangelsdorf <luanferreira2136@gmail.com>
2021-08-24 14:13:41 -0300

    Contact animation

--- src/components/pitch/Contact/index.js
+++ src/components/pitch/Contact/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { constructSequentialAnimation, handleIntersection } from 'src/modules/App';
 import styles from './Contact.module.scss';
 
 export default function Contact() {
+  const element = useRef(null);
+  const refs = [element.current];
+  const [animations, setAnimations] = useState(null);
+
+  useEffect(() => {
+    let targets = Array.from(element.current.children);
+    let opt = {
+      duration: 2000,
+      easing: 'ease',
+      fill: 'both',
+      delay: 0
+    };
+    let keyframes = {
+      opacity: [0, 1],
+      transform: ['translateY(-50px)', 'initial']
+    };
+
+    let textAnimation = constructSequentialAnimation(targets, keyframes, opt, 600)
+    setAnimations([textAnimation]);
+  }, [])
+
+  useEffect(() => {
+    if (animations) {
+      handleIntersection(refs, animations);
+    }
+  }, [animations])
+
   return (
     <section className={styles.section} id="contact">
       <div className="container">
-        <div className="row">
+        <div className="row" ref={element}>
           <div className="col-12 col-lg-10 mx-auto">
             <div className={styles.contact}>
               <div>
