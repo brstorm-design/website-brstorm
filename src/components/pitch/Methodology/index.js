c7fc5a018444a6fd1b814fcb62e261bc0c001cc2
luangelsdorf <luanferreira2136@gmail.com>
2021-10-25 17:15:08 -0300

    Fixed step numbers

--- src/components/pitch/Methodology/index.js
+++ src/components/pitch/Methodology/index.js
@@ -77,6 +77,7 @@ export default function Methodology({ content }) {
                   content.steps.map((step, index) => {
                     return (
                       <div key={`step-${index}`}>
+                        <span className={styles.number}>{index + 1}</span>
                         <img src={`/images/wireframes/${step.icon}`} alt="" />
                         <h5>{step.name}</h5>
                         <small>{step.text}</small>
