763f86de7bb369fbdba6d69ee6ca5e7b303c6bab
luangelsdorf <luanferreira2136@gmail.com>
2021-12-14 18:10:45 -0300

    Fixed timeline

--- src/components/pitch/Methodology/index.js
+++ src/components/pitch/Methodology/index.js
@@ -1,71 +1,62 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Samothrace from 'src/components/illustrations/Samothrace';
-import { getTranslateValue } from 'src/modules/App';
+import { getMaxHeight, getTranslateValue } from 'src/modules/App';
 import styles from './Methodology.module.scss';
 
 export default function Methodology({ content }) {
   const [mousePosition, setMousePosition] = useState(null);
-
-  let thres = [];
-  for (let index = 0; index <= 1.0; index = index + 0.01) {
-    thres.push(index);
-  }
-
-  const lineOptions = {
-    rootMargin: '10000px 0px -50% 0px',
-    threshold: thres,
-  }
-  const activeOptions = {
-    rootMargin: '-20% 0px -66% 0px',
-  }
-
-  useEffect(() => {
-    /* line IntersectionObserver */
-    const lineObserver = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.nextElementSibling.style.height = `calc(${entry.intersectionRatio * 100}% * 8.6)`;
-        }
-      })
-    }, lineOptions);
-
-    const line = document.querySelector(`.${styles.line}.lineBase`);
-    lineObserver.observe(line);
-
-    /* active class IntersectionObserver */
-    const activeObserver = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add(styles.active);
-        } else {
-          entry.target.classList.remove(styles.active);
-        }
-      })
-    }, activeOptions);
-
-    let cardList = document.querySelectorAll(`.${styles.cards} > div`);
-    cardList.forEach(card => {
-      activeObserver.observe(card);
-    })
-
-    return () => {
-      lineObserver.unobserve(line);
-      cardList.forEach(card => activeObserver.unobserve(card))
-    }
-  }, [])
+  const [lineHeight, setLineHeight] = useState(0);
+  const line = useRef(null);
 
   function Line() {
-    return (
-      <>
-        <div className={`${styles.line} lineBase`} />
-        <div className={styles.line} />
-      </>
-    )
+    return <div ref={line} style={{ height: `${lineHeight}px` }} className={styles.line} />
   }
 
   function move(e) {
     setMousePosition(getTranslateValue(e, [-40, 40]));
   }
+
+  let screenCenter;
+  let target;
+  let maxLineHeight;
+  let numbers;
+
+  function handleScroll() {
+    const el = document.querySelector(`.${styles.cards}`);
+    const top = el.getBoundingClientRect().top;
+    const lineBottom = line.current.getBoundingClientRect().bottom;
+    let height = screenCenter - top;
+    if (top < screenCenter && height < maxLineHeight) {
+      setLineHeight(height);
+    }
+
+    numbers.forEach((number, index) => {
+      let margin;
+      index === 0 ? margin = 50 : margin = 0;
+      // 👆 adicionando uma margem de 100px para o primeiro elemento ficar ativo
+      if (number.getBoundingClientRect().top + margin < lineBottom) {
+        number.parentElement.classList.add(styles.active);
+      } else {
+        number.parentElement.classList.remove(styles.active);
+      }
+    });
+  }
+
+  useEffect(() => {
+    const cardList = document.querySelectorAll(`.${styles.cards} > div`);
+    //
+    screenCenter = window.innerHeight / 2;
+    target = document.querySelector(`.${styles.line}:last-child`);
+    maxLineHeight = getMaxHeight(cardList);
+    numbers = document.querySelectorAll(`.${styles.cards} > div span`);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [screenCenter, target]);
 
   return (
     <section className={styles.section} onMouseMove={move}>
@@ -85,7 +76,7 @@ export default function Methodology({ content }) {
                   content.steps.map((step, index) => {
                     return (
                       <div key={`step-${index}`}>
-                        <span className={styles.number}>{index + 1}</span>
+                        <span id={`step-${index + 1}`} className={styles.number}>{index + 1}</span>
                         {/* <img src={`/images/wireframes/img-icon.svg`} alt="" /> */}
                         <h5>{step.name}</h5>
                         <small>{step.text}</small>
--- src/modules/App.js
+++ src/modules/App.js
@@ -87,3 +87,16 @@ export function applyStyles(targets = [], translate = []) {
     }
   })
 }
+
+export function getMaxHeight(elements = NodeList) {
+  let height = 0;
+  elements.forEach((element, index) => {
+    if (index === 0 || index === elements.length - 1) {
+      height += (element.clientHeight / 2);
+    } else {
+      height += element.clientHeight;
+    }
+  });
+  height += 50 * (elements.length - 1);
+  return height;
+}
