c96270f2d572ed97bbdb07138a5c0931e5e582c1
luangelsdorf <luanferreira2136@gmail.com>
2022-03-01 19:23:55 -0300

    Fixed JSX props

--- src/components/presentation/Slides/index.js
+++ src/components/presentation/Slides/index.js
@@ -3,6 +3,6 @@ import styles from './Slides.module.scss';
 
 export default function Slides() {
   return (
-    <iframe className={styles.frame} src="https://docs.google.com/presentation/d/e/2PACX-1vSqs1mW_i3UB5a04zKZscw2hvS4xyDKka8KvDDRHNjdxarFHsXRRXl-7nJyMSp0w0iQfi_4CCY0pZQ7/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="629" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+    <iframe className={styles.frame} src="https://docs.google.com/presentation/d/e/2PACX-1vSqs1mW_i3UB5a04zKZscw2hvS4xyDKka8KvDDRHNjdxarFHsXRRXl-7nJyMSp0w0iQfi_4CCY0pZQ7/embed?start=false&loop=false&delayms=3000" frameBorder="0" width="960" height="629" allowFullScreen="true" mozallowFullScreen="true" webkitallowFullScreen="true"></iframe>
   )
 }
