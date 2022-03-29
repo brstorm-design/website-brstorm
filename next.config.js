13b69cb715bf443eace5d93f90c70c26aa300065
luangelsdorf <luanferreira2136@gmail.com>
2022-03-28 21:46:40 -0300

    Subdomain test

--- next.config.js
+++ next.config.js
@@ -3,16 +3,35 @@ module.exports = {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: [{ loader: '@svgr/webpack', options: {svgo: false} }],
+      use: [{ loader: '@svgr/webpack', options: { svgo: false } }],
     })
 
     return config
   },
-  
+
   i18n: {
     locales: ['en', 'pt'],
     defaultLocale: 'en',
     localeDetection: true,
+  },
+
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `app.acme.com`,
+        // this rewrite will be applied
+        {
+          source: '/:path*',
+          has: [
+            {
+              type: 'host',
+              value: 'brand.brstorm.design',
+            },
+          ],
+          destination: '/brand/:path*',
+        },
+      ]
+    }
   },
 
   reactStrictMode: true,
