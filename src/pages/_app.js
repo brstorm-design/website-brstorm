60b5171910306766dec735d440442607791a73ca
luangelsdorf <luanferreira2136@gmail.com>
2022-04-10 15:20:35 -0300

    Changed backrounds to variables

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,19 @@ import '../../public/css/bootstrap.css';
 import 'src/styles/styles.scss';
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
+import { useEffect } from 'react';
+import { rootPath } from 'src/utils/env';
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    document.body.style.setProperty('--background-detail', `
+    url("${rootPath}/images/backgrounds/cover/left.svg"),
+    url("${rootPath}/images/backgrounds/cover/right.svg"),
+    url("${rootPath}/images/backgrounds/footer.svg")
+    `);
+  }, []);
+
   return (
     <>
       <Head>
