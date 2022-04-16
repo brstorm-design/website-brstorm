ceeef23eae3a1ed0eadd9f8ce1ac40a908724489
luangelsdorf <luanferreira2136@gmail.com>
2022-04-16 09:07:38 -0300

    Added TextInput styles

--- src/components/form/TextInput/index.js
+++ src/components/form/TextInput/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './TextInput.module.scss';
 
-export default function TextInput({ name, type, placeholder, required }) {
+export default function TextInput({ name, placeholder, required }) {
+  const [height, setHeight] = useState('auto');
+  const field = useRef(null);
   const props = arguments[0];
-  
+
+  function resize() {
+    setHeight('auto');
+    setHeight(`${field.current.scrollHeight}px`);
+  }
+
+  useEffect(() => {
+    document.querySelector('textarea').addEventListener('input', resize, false);
+    return () => document.querySelector('textarea').removeEventListener('input', resize, false);
+  }, [])
+
   return (
-    <input {...props} className={styles.field} id={name} />
+    <textarea
+      {...props}
+      ref={field}
+      rows="1"
+      className={styles.field}
+      style={{ height: height }}
+    />
   )
 }
