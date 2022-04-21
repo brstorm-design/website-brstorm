672b2c976b6475d09ad1fd7336613dedd326a30b
luangelsdorf <luanferreira2136@gmail.com>
2022-04-20 21:21:08 -0300

    Added container on Web Pitch Cover

--- src/components/pitch/Cover/index.js
+++ src/components/pitch/Cover/index.js
@@ -6,7 +6,7 @@ import styles from './Cover.module.scss';
 export default function Cover() {
   return (
     <section className={styles.section}>
-      <div>
+      <div className="container">
         <h1>Hi, Fabio and Sara!</h1>
         <h2>We will help Bronzo grow.</h2>
         <div>{'We prepared a brief presentation for you to better\n know us and the solutions we provide.'}</div>
@@ -15,7 +15,6 @@ export default function Cover() {
           <svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.8333 6L9.89332 5.06L6.16666 8.78V0.666668H4.83332V8.78L1.11332 5.05333L0.166656 6L5.49999 11.3333L10.8333 6Z" fill="#555" /></svg>
         </AnchorButton>
       </div>
-      {/* <img src="/images/wireframes/mosaic.png" alt="" /> */}
     </section>
   )
 }
