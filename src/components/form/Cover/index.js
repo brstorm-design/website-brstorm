f8a2c9d3001269d6b8cc517ab5ba6629d02cd6bb
luangelsdorf <luanferreira2136@gmail.com>
2022-05-17 19:39:59 -0300

    Mobile version spacings

--- src/components/form/Cover/index.js
+++ src/components/form/Cover/index.js
@@ -4,10 +4,16 @@ import styles from './Cover.module.scss';
 export default function Cover({ content }) {
   return (
     <section className={styles.section}>
-      <div>
-        <h3 className="gradient-bg">{content.subTitle}</h3>
-        <h1>{content.title}</h1>
-        <small>{content.timeToFill}</small>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className={styles.text}>
+              <h4 className="gradient-bg">{content.subTitle}</h4>
+              <h1>{content.title}</h1>
+              <small>{content.timeToFill}</small>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   )
