d5aa431f88cc66621266693bf52c8ce124d32f33
luangelsdorf <luanferreira2136@gmail.com>
2022-05-30 16:38:41 -0300

    Recent Projects component

--- src/components/portfolio/RecentProjects/index.js
+++ src/components/portfolio/RecentProjects/index.js
@@ -1,32 +1,91 @@
-import Link from 'next/link';
 import React from 'react';
 import { rootPath } from 'src/utils/env';
 import styles from './RecentProjects.module.scss';
-import Arrow from 'public/images/arrow-forward.svg';
 import SeeMore from 'src/components/common/SeeMore';
+import { Swiper, SwiperSlide } from 'swiper/react';
+//
+import daia from 'public/images/portfolio/daia.png';
+import catarge from 'public/images/portfolio/catarge.png';
+import petsvida from 'public/images/portfolio/petsvida.png';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function RecentProjects() {
+  const portfolio = [
+    {
+      id: 1,
+      name: 'Daia',
+      cover: daia,
+    },
+    {
+      id: 2,
+      name: 'Catarge',
+      cover: catarge,
+    },
+    {
+      id: 3,
+      name: 'PetsVida',
+      cover: petsvida,
+    },
+  ]
+
   return (
     <section className={styles.section}>
       <div className="container">
-        <div className={styles.title}>
-          <h1>Projetos Recentes</h1>
-          <SeeMore linkText="Ver Mais" href="/portfolio" className="d-none d-md-block">
-            Portfólio Completo
-          </SeeMore>
-        </div>
-        <div className="row gy-4">
-          <div className="col-12 col-lg-6">
-            <div className={styles.placeholder}>
-              <img src={`${rootPath}/images/wireframes/img-icon.svg`} alt="" />
+        <div className="row justify-content-between">
+          <div className="col offset-lg-1">
+            <div className={styles.title}>
+              <h1>Projetos Recentes</h1>
             </div>
           </div>
-          <div className="col-12 col-lg-6">
-            <div className={styles.placeholder}>
-              <img src={`${rootPath}/images/wireframes/img-icon.svg`} alt="" />
-            </div>
+          <div className="col-md-4 d-flex justify-content-end">
+            <SeeMore linkText="Ver Mais" href="/portfolio" className="d-none d-md-block">
+              Portfólio Completo
+            </SeeMore>
           </div>
+          <div className="col-lg-1"></div>
         </div>
+
+        <Swiper spaceBetween={24} slidesPerView="auto" className="d-none d-md-block">
+          {
+            portfolio.map(item => {
+              return (
+                <SwiperSlide key={`project-${item.id}`}>
+                  <Link href="#">
+                    <a className={styles.project}>
+                      <Image objectFit="cover" placeholder="blur" src={item.cover} layout="responsive" />
+                      <div>
+                        <h5>{item.name}</h5>
+                        <span className="details">{item.type}</span>
+                      </div>
+                    </a>
+                  </Link>
+                </SwiperSlide>
+              )
+            })
+          }
+        </Swiper>
+
+        <div className={styles.mobileProjects}>
+          {
+            portfolio.map(item => {
+              return (
+                <div className="" key={`project-mobile-${item.id}`}>
+                  <Link href="#">
+                    <a className={styles.project}>
+                      <Image objectFit="cover" placeholder="blur" src={item.cover} layout="responsive" />
+                      <div>
+                        <h5>{item.name}</h5>
+                        <span className="details">{item.type}</span>
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              )
+            })
+          }
+        </div>
+
       </div>
       <SeeMore linkText="Ver Mais" href="/portfolio" className="d-block d-md-none text-center">
         Portfólio Completo
