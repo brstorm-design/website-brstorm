1325a4ef5e0b75bf6ca4fea4eecabc7663b1e692
luangelsdorf <luanferreira2136@gmail.com>
2022-06-23 17:05:46 -0300

    Desktop UI animation

---
+++ src/components/svg/DesktopUI/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './DesktopUI.module.scss';
+import InterfaceDesktop from 'public/images/cover/web/desktop.svg';
+
+export default function DesktopUI() {
+  return (
+    <InterfaceDesktop className={styles.interface} />
+  )
+}
