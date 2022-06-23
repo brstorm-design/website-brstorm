0f9412fbc2b4ce0c9b788dcaea374798e5300893
luangelsdorf <luanferreira2136@gmail.com>
2022-06-23 16:05:54 -0300

    Mobile UI animation

---
+++ src/components/svg/MobileUI/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './MobileUI.module.scss';
+import Interface from 'public/images/cover/web/mobile.svg';
+
+export default function MobileUI() {
+  return (
+    <Interface className={styles.interface} />
+  )
+}
