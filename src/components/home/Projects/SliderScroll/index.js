10c156ab73534788d02d0447c2f855b057809b12
luangelsdorf <luanferreira2136@gmail.com>
2022-07-18 09:32:34 -0300

    Added SliderScroll component

---
+++ src/components/home/Projects/SliderScroll/index.js
@@ -0,0 +1,87 @@
+import Image from 'next/image';
+import React, { useContext, useEffect, useRef } from 'react';
+import images from 'src/utils/images';
+import styles from './SliderScroll.module.scss';
+import SeeMore from 'src/components/common/SeeMore';
+import { SmoothScrollContext } from 'src/contexts/SmoothScrollContext';
+
+export default function SliderScroll({ content, common }) {
+  const spaceHolder = useRef(null);
+  const sliderElement = useRef(null);
+
+  const { scroll } = useContext(SmoothScrollContext);
+
+  useEffect(() => {
+    document.body.classList.add('page');
+
+    function calcDynamicHeight(sliderElement) {
+      const vw = window.innerWidth; // 3605
+      const vh = window.innerHeight;
+      const objectWidth = sliderElement.scrollWidth;
+      const dynHeight = objectWidth - vw + vh + 120;
+      return dynHeight;
+    }
+
+    function onSmoothScroll(e) {
+      if (e.currentElements.el0) {
+        sliderElement.current.style.transform = `translate3d(-${e.scroll.y - initialOffsetTop}px, 0, 0)`;
+      }
+    }
+
+    const initialOffsetTop = spaceHolder.current.getBoundingClientRect().top;
+    spaceHolder.current.style.height = `${calcDynamicHeight(sliderElement.current)}px`;
+
+    scroll?.on('scroll', onSmoothScroll);
+    return () => {
+      scroll?.off('scroll', onSmoothScroll);
+    }
+  }, [scroll]);
+
+
+  return (
+    <>
+      {/* <div className={styles.bumper}>
+        Scroll Down
+      </div> */}
+
+      <div className={styles.spaceHolder} ref={spaceHolder} id="space-holder">
+        <div
+          data-scroll
+          data-scroll-repeat
+          data-scroll-sticky
+          data-scroll-target="#space-holder"
+          className={styles.sticky}
+        >
+          <div className="container">
+            <div className={styles.cards} ref={sliderElement}>
+              {
+                content.portfolio.map((item, index) => {
+                  return (
+                    <div key={`projects-${index}`}>
+                      <a href={item.url} rel="noopener noreferrer" target="_blank" className={styles.link}>
+                        <Image src={images[item.slug]} layout="responsive" objectFit="cover" />
+                        <div id="overlay-details">
+                          <h5>{item.name}</h5>
+                          <span className="details">{item.type}</span>
+                        </div>
+                      </a>
+                    </div>
+                  )
+                })
+              }
+              <div>
+                <SeeMore linkText={common.seeMoreProjects} href={`${content.url}`}>
+                  {content.fullPortfolio}
+                </SeeMore>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className={styles.bumper}>
+        Scroll Up
+      </div> */}
+    </>
+  )
+}
