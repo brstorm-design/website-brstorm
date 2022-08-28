b9f39b6608f6fb7d878543e3817e467c3a106299
luangelsdorf <luanferreira2136@gmail.com>
2022-08-27 21:20:38 -0300

    Changed project slider speed

--- src/components/home/Projects/Slider/index.js
+++ src/components/home/Projects/Slider/index.js
@@ -30,7 +30,7 @@ export default function Slider({ portfolio }) {
           slidesPerView={'auto'}
           modules={[Autoplay, FreeMode]}
           loop
-          speed={800}
+          speed={1800}
           freeMode
           autoplay={{
             delay: 200,
@@ -64,7 +64,7 @@ export default function Slider({ portfolio }) {
           slidesPerView={'auto'}
           modules={[Autoplay, FreeMode]}
           loop
-          speed={800}
+          speed={1800}
           freeMode
           autoplay={{
             delay: 200,
