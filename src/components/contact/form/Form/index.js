1a3218b16149543189c9315cea1b2ec3fffa9266
luangelsdorf <luanferreira2136@gmail.com>
2022-09-03 14:58:19 -0300

    Contact form auto-send

--- src/components/contact/form/Form/index.js
+++ src/components/contact/form/Form/index.js
@@ -11,6 +11,7 @@ import styles from './Form.module.scss';
 
 export default function Form({ fields, submitText, values, setValues, handleFieldSetChange, activeField }) {
   const [loading, setLoading] = useState(false);
+  const [infosSent, setInfosSent] = useState(false);
   const router = useRouter();
 
   const { scroll } = useContext(SmoothScrollContext);
@@ -61,6 +62,57 @@ export default function Form({ fields, submitText, values, setValues, handleFiel
     }
   }
 
+  /*** automatically populate Infos form ***/
+  useEffect(() => {
+    if (infosSent) return;
+    function handleFocusOut() {
+      const contactFields = [values['entry.264212494'], values['entry.2132000314'], values['entry.41791649']];
+      const hasFilledContact = contactFields.some(field => field.length > 0);
+
+      if (values['entry.1247986906'] && hasFilledContact) {
+        const [filledContactField] = contactFields.filter(field => field.length > 0);
+        const queryString = getQueryString({
+          'entry.576692421': values['entry.1247986906'],
+          'entry.1100959596': filledContactField,
+        });
+
+        fetch('https://docs.google.com/forms/d/e/1FAIpQLSdS4TZP9lxgDBhsrAu9QhuefgJJ73Dc9RKxwn5JT4XSq-N37g/formResponse', {
+          method: 'POST',
+          mode: 'no-cors',
+          headers: {
+            'Accept': '*/*',
+            'Content-Type': 'application/x-www-form-urlencoded',
+          },
+          body: queryString,
+        })
+          .then(res => {
+            console.log(res);
+            setInfosSent(true);
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      }
+    }
+
+    const infoElements = document.querySelectorAll('#name, #yourContact');
+    infoElements.forEach(element => {
+      element.addEventListener('focusout', handleFocusOut);
+    });
+
+    return () => {
+      infoElements.forEach(element => {
+        element.removeEventListener('focusout', handleFocusOut);
+      });
+    }
+  }, [
+    infosSent,
+    values['entry.1247986906'],
+    values['entry.264212494'],
+    values['entry.2132000314'],
+    values['entry.41791649']
+  ]); // 👆 deps: name field and the 3 contact fields
+
   return (
     <div className="container">
       <div className="row">
@@ -75,7 +127,7 @@ export default function Form({ fields, submitText, values, setValues, handleFiel
                     helperText={field.helperText}
                     required={field.attributes.required}
                     type={field.attributes.type}
-                    name={field.attributes.name === 'entry.1868660285' ? values['entry.1247986906'] : null}
+                    name={field.attributes.name === 'yourContact' ? values['entry.1247986906'] : null}
                     id={field.sectionName}
                     active={activeField?.id === field.attributes.name ? true : false}
                   >
