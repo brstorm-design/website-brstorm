60cc6401f731676f12c4d1b5b4991dd9256e0f51
luangelsdorf <luanferreira2136@gmail.com>
2022-09-07 15:13:41 -0300

    Updated Analytics tags

--- src/components/scripts/GoogleAnalytics.js
+++ src/components/scripts/GoogleAnalytics.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default function GoogleAnalytics() {
   return (
     <>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-KQGH4BZSRJ"/>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-TR9HJZQVSH"/>
       <script dangerouslySetInnerHTML={{
         __html:
           `window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-KQGH4BZSRJ');`
+          gtag('config', 'G-TR9HJZQVSH');`
       }}/>
     </>
   )
