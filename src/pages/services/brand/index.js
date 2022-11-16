c2eba9ca894d8607969dfcfcf4c86bdb7691cbb8
luangelsdorf <luanferreira2136@gmail.com>
2022-11-16 16:05:24 -0300

    Added `data-scroll-sections`

--- src/pages/services/brand/index.js
+++ src/pages/services/brand/index.js
@@ -47,38 +47,34 @@ export default function LandingPage({ content, service }) {
 
       <WhatsAppFixed />
 
-      <div className="main" data-scroll-section>
-        <main>
-          <Section pt="220">
-            <PitchCover content={landing.cover} common={common} />
-          </Section>
+      <Section data-scroll-section pt="220">
+        <PitchCover content={landing.cover} common={common} />
+      </Section>
 
-          <Section mt="165 180">
-            <Details content={landing.details} />
-          </Section>
+      <Section data-scroll-section mt="165 180">
+        <Details content={landing.details} />
+      </Section>
 
-          <Section mt="235 200">
-            <Projects layout="scroll" content={landing.projects} allProjects={fullPortfolio[service.jsonName]} common={common} />
-          </Section>
+      <Section data-scroll-section mt="235 200">
+        <Projects layout="scroll" content={landing.projects} allProjects={fullPortfolio[service.jsonName]} common={common} />
+      </Section>
 
-          <Section mt="240 200">
-            <Methodology content={landing.method} />
-          </Section>
+      <Section data-scroll-section mt="240 200">
+        <Methodology content={landing.method} />
+      </Section>
 
-          <Section mt="200">
-            <WhyUs content={home.whyUs} />
-          </Section>
+      <Section data-scroll-section mt="200">
+        <WhyUs content={home.whyUs} />
+      </Section>
 
-          <Section mt="200">
-            <Testimonials content={home.testimonials} />
-          </Section>
+      <Section data-scroll-section mt="200">
+        <Testimonials content={home.testimonials} />
+      </Section>
 
-          <Section mt="200" mb="120 200">
-            <Contact content={content.pitch.contact} common={common} />
-          </Section>
-        </main>
-        <Footer content={home.footer} common={common} />
-      </div>
+      <Section data-scroll-section mt="200" mb="120 200">
+        <Contact content={content.pitch.contact} common={common} />
+      </Section>
+      {/* <Footer content={home.footer} common={common} /> */}
     </>
   )
 }
