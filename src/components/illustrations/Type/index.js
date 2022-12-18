86fb62ea5f49d54b7e77ac9d9405b232673a329f
luangelsdorf <luanferreira2136@gmail.com>
2022-12-17 23:36:12 -0300

    Random logo animation

--- src/components/illustrations/Type/index.js
+++ src/components/illustrations/Type/index.js
@@ -1,57 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import letters from './letters';
 import styles from './Type.module.scss';
 
-const word = ['B', 'R', 'Dot', 'S', 'T', 'O', 'R', 'M'];
+export default function Type({ single = false }) {
+  const [docVisibility, setDocVisibility] = useState('visible');
+  const wrapper = useRef(null);
+  let combination = Array(8).fill(0).map(() => getRandomInt(7));
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-function getRandomIntInRange(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
+  const word = ['B', 'R', 'Dot', 'S', 'T', 'O', 'R', 'M'];
 
-const keyframesIn = [
-  {
-    opacity: 0,
-    transform: 'rotateX(180deg)',
-  },
-  {
-    opacity: 1,
-    transform: 'rotateX(360deg)',
-  },
-];
-const keyframesOut = [
-  {
-    opacity: 1,
-    transform: 'rotateX(0deg)',
-  },
-  {
-    opacity: 0,
-    transform: 'rotateX(180deg)',
-  },
-];
-const options = { duration: 500, fill: 'both' };
-
-function updateLetter(charToChange, nextVariantIndex, reflow) {
-  const charElement = document.querySelectorAll('[data-letter]')[charToChange];
-  const currentVariant = charElement.querySelector(`.${styles.visible}`);
-  const nextVariant = charElement.children[nextVariantIndex];
-
-  if (reflow) {
-    charElement.style.width = nextVariant.querySelector('svg').width.baseVal.value + 'px';
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  function getRandomIntInRange(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
   }
 
-  charElement.animate(keyframesOut, { easing: 'ease-in', ...options }).finished.then(() => {
-    currentVariant?.classList.remove(styles.visible);
-    nextVariant?.classList.add(styles.visible);
-    charElement.animate(keyframesIn, { easing: 'ease-out', ...options });
-  });
-}
+  const keyframesIn = [
+    {
+      opacity: 0,
+      transform: 'rotateX(180deg)',
+    },
+    {
+      opacity: 1,
+      transform: 'rotateX(360deg)',
+    },
+  ];
+  const keyframesOut = [
+    {
+      opacity: 1,
+      transform: 'rotateX(0deg)',
+    },
+    {
+      opacity: 0,
+      transform: 'rotateX(180deg)',
+    },
+  ];
+  const options = { duration: 500, fill: 'both' };
 
-export default function Type() {
-  const [docVisibility, setDocVisibility] = useState('visible');
-  let combination = Array(8).fill(0).map(() => getRandomInt(7));
+  function updateLetter(charToChange, nextVariantIndex, reflow) {
+    const charElement = wrapper.current.querySelectorAll('[data-letter]')[charToChange];
+    const currentVariant = charElement.querySelector(`.${styles.visible}`);
+    const nextVariant = charElement.children[nextVariantIndex];
+
+    charElement.animate(keyframesOut, { easing: 'ease-in', ...options }).finished.then(() => {
+      currentVariant?.classList.remove(styles.visible);
+      if (reflow) {
+        charElement.style.width = nextVariant.querySelector('svg').width.baseVal.value + 'px';
+      }
+      nextVariant?.classList.add(styles.visible);
+      charElement.animate(keyframesIn, { easing: 'ease-out', ...options });
+    });
+  }
 
   useEffect(() => {
     document.querySelectorAll('[data-letter]').forEach(letter => {
@@ -68,11 +68,13 @@ export default function Type() {
       const letterVariantArray = letters[charToChange];
       const nextCombination = combination.map((item, i) => getRandomInt(letters[word[i]].length));
 
-      /* nextCombination.forEach((variant, index) => {
-        updateLetter(index, variant, false);
-      }); */
-
-      updateLetter(randomIndex, getRandomInt(letterVariantArray.length), true);
+      if (single) {
+        updateLetter(randomIndex, getRandomInt(letterVariantArray.length), true);
+      } else {
+        nextCombination.forEach((variant, index) => {
+          updateLetter(index, variant, true);
+        });
+      }
 
       if (document.visibilityState === 'visible') letterChangeTimeout = setTimeout(changeLetter, 3000);
     }
@@ -90,8 +92,7 @@ export default function Type() {
 
   return (
     <div>
-      <h1>Type</h1>
-      <div className={styles.logotype}>
+      <div className={styles.logotype} ref={wrapper}>
         {
           word.map((char, charIndex) => {
             return (
--- src/pages/random-logo.js
+++ src/pages/random-logo.js
@@ -10,13 +10,23 @@ export default function PriceCirlcePage() {
 
   return (
     <div style={{
-      display: 'block',
-      height: '5000px',
+      display: 'flex',
+      flexWrap: 'wrap',
+      gap: '48px',
+      justifyContent: 'space-evenly',
+      width: '100%',
       padding: '32px',
     }}
       className="main"
     >
-      <Type />
+      <div>
+        <h2>Type Animation 1</h2>
+        <Type key={1} />
+      </div>
+      <div>
+        <h2>Type Animation 2</h2>
+        <Type key={2} single />
+      </div>
     </div>
   )
 }
